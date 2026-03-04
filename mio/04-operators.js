264f226f2f8865b350820ab1a0afb271649825af
Hajime Shiroma <hajishiro.hs@gmail.com>
2026-03-04 17:56:33 -0300

    Agregar archivo de operadores con ejemplos de aritmética, asignación y comparación

---
+++ mio/04-operators.js
@@ -0,0 +1,66 @@
+// Operadores
+
+// Operadores aritmeticos
+
+let a = 5
+let b = 10
+
+console.log(a + b) // Suma
+console.log(a - b) // Resta
+console.log(a * b) // Multiplicacion
+console.log(a / b) // Division 
+
+console.log(a % b) // Modulo
+console.log(a ** b) // Exponencial
+
+a++ // Incremento
+console.log(a)
+
+b-- // Decremento
+console.log(b)
+
+// Operadores de asignacion
+
+let myVariable = 2
+console.log(myVariable)
+myVariable += 2 // myVariable = myVariable + 2
+console.log(myVariable)
+
+myVariable -= 2 // myVariable = myVariable - 2
+console.log(myVariable)
+myVariable *= 2 // myVariable = myVariable * 2
+console.log(myVariable)
+myVariable /= 2 // myVariable = myVariable / 2
+console.log(myVariable)
+myVariable %= 3 // myVariable = myVariable % 2
+console.log(myVariable)
+myVariable **= 2 // myVariable = myVariable ** 2
+console.log(myVariable)
+
+// Operadores de comparacion
+
+console.log(a)
+a = 5
+
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+
+// Igualdad por valor
+
+console.log(a == b)
+console.log(a == a)
+console.log(a == 5)
+console.log(a == '5')
+
+// Igualdada por identidad (tipo y valor)
+
+console.log(a === a)
+console.log(a === 5)
+console.log(a === '5')
+
+// Desigualdad
+
+console.log(a != 5)
+console.log(a !== '5')
