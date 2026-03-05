27136873663d9954bf08533ed08c54a8015c9f87
Hajime Shiroma <hajishiro.hs@gmail.com>
2026-03-04 21:45:18 -0300

    Agregar comentarios explicativos en ejemplos de operadores de comparación y lógicos

--- mio/04-operators.js
+++ mio/04-operators.js
@@ -56,11 +56,47 @@ console.log(a == '5')
 
 // Igualdada por identidad (tipo y valor)
 
-console.log(a === a)
-console.log(a === 5)
-console.log(a === '5')
+console.log(a === a) // true
+console.log(a === 5) // true
+console.log(a === '5') // false
 
 // Desigualdad
 
 console.log(a != 5)
 console.log(a !== '5')
+
+// Cosas raras
+
+console.log(0 == false) // true
+console.log(1 == false) // false
+
+console.log(0 == '') // true
+console.log(0 == " ") // true
+console.log(0 == 'hola') //false
+
+console.log(0 === '') // false
+
+console.log(undefined == null) // true
+console.log(undefined === null) // false
+
+// Operadores logicos
+
+// and (&&)
+console.log(5 > 10 && 15 > 20) // false
+console.log(5 < 10 && 15 < 20) // true
+console.log(5 < 10 && 15 > 20) // false
+
+// or (||)
+console.log(5 > 10 || 15 > 20) // false
+console.log(5 < 10 || 15 < 20) // true
+console.log(5 < 10 || 15 > 20) // true
+
+// not (!)
+console.log(!(5 > 10 && 15 > 20)) // true
+console.log(!(5 > 10 || 15 > 20)) // true
+
+// Operadores ternarios
+
+const isRaining = true
+
+isRaining ? console.log('Esta lloviendo') : console.log('No esta lloviendo')
